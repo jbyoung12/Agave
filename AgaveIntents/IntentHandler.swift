ce9c379e77f308044690b27748d1e17ffeddda24
Oscar Hong <oscar.hong2015@gmail.com>
2018-10-14 17:01:57 -0400

    austin's latest sirikit attempt

--- AgaveIntents/IntentHandler.swift
+++ AgaveIntents/IntentHandler.swift
@@ -5,17 +5,8 @@
 //  Created by Oscar Hong on 10/14/18.
 //  Copyright © 2018 JBYoung. All rights reserved.
 //
-
+//import Firebase
 import Intents
-
-// As an example, this class is set up to handle Message intents.
-// You will want to replace this or add other intents as appropriate.
-// The intents you wish to handle must be declared in the extension's Info.plist.
-
-// You can test your example integration by saying things to Siri like:
-// "Send a message using <myApp>"
-// "<myApp> John saying hello"
-// "Search for messages in <myApp>"
 
 class IntentHandler: INExtension{
     
@@ -30,12 +21,29 @@ class IntentHandler: INExtension{
     
     func handle(intent: CreateBookmarkIntent, completion: @escaping (CreateBookmarkIntentResponse) -> Void) {
         
+//        let db = Firestore.firestore()
         
+
         print("Handling voice response")
         
-        
-//        var a = SoundViewController()
-//        a.addBookmark(self)
+        //let time = Player.app.audioPlayer?.currentTime
+        /*
+//       let podcastUsers = db.collection("podcasts").document((Player.app.podcastId)!).collection("users")
+//        let edwardBookmarks = podcastUsers.document("VJUKEVptSyfxFrvnCXKg").collection("bookmarks")
+//        var ref: DocumentReference? = nil
+//        ref = edwardBookmarks.addDocument(data: [
+//            "time": time!,
+//            "description": ""
+//        ]) {
+//            err in
+//            if let err = err {
+//                print("Error adding document: \(err)")
+//            } else {
+//                print("Document added with ID: \(ref!.documentID)")
+//            }
+//        }
+//        let a = SoundViewController()
+//        a.addBookmark(self)*/
         //return success
         completion(CreateBookmarkIntentResponse(code: .success, userActivity: nil))
         
